01e233d2a160622e00f57270596e58bd7f32309d
jbordoni <jackbordoni@gmail.com>
2017-02-28 14:12:20 -0500

    added .json endpoint for users and plants

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ admin.initializeApp({
 var db = admin.database();
 var ref = db.ref("/");
 var plantsRef = ref.child("plants");
+var usersRef = ref.child("users");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-var plantsArr=[];
+var plantsArr = [];
+var usersArr = [];
 
 plantsRef.orderByChild("plantname").on("child_added", function(snapshot, prevChildKey) {
 	var plantname = snapshot.val().plantname;
@@ -31,9 +33,21 @@ plantsRef.orderByChild("plantname").on("child_added", function(snapshot, prevChi
 	plantsArr.push(plantEntry);
 });
 
+usersRef.orderByChild("username").on("child_added", function(snapshot, prevChildKey) {
+	var userEntry = {
+		username: snapshot.val().username
+	};
+	usersArr.push(userEntry);
+});
 
 app.get("/", function(req, res){
  	res.render("landing.ejs", {plantsArr: plantsArr});
+});
+
+
+//JSON ENDPOINT FOR ALL PLANT DATA
+app.get("/.json", function(req,res){
+	res.send(JSON.stringify(plantsArr));
 });
 
 app.get("/newplant", function(req, res){
@@ -50,6 +64,28 @@ app.post("/newplant", function(req, res){
 		imageurl: url
 	});
 	res.redirect("/");
+});
+
+app.get("/newuser", function(req, res){
+	res.render("newuser.ejs");
+});
+
+app.post("/newuser", function(req,res){
+	var username = req.body.username;
+	var password = req.body.password;
+	usersRef.push({
+		username: username,
+		password: password
+	});
+	res.redirect("/users");
+});
+
+app.get("/users", function(req, res){
+	res.render("users.ejs", {usersArr: usersArr});
+});
+
+app.get("/users.json", function(req,res){
+	res.send(JSON.stringify(usersArr));
 });
 
 app.listen(3000, function(){
