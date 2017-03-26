93fc36caa9dd051d78fff7210191378417b3c8ed
jbordoni <jackbordoni@gmail.com>
2017-03-25 22:21:52 -0400

    new structure
    can add new plant, locate plants on map, sign in, sign out, sign up

--- app.js
+++ app.js
@@ -1,112 +1,252 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var admin = require("firebase-admin");
 
-var serviceAccount = require("./accountkey.json");
+var dbRef = firebase.database().ref();
+var plantsRef = firebase.database().ref('plants');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://scavenger-7165d.firebaseio.com"
+var storage = firebase.storage();
+var storageRef = storage.ref();
+var photosRef = storageRef.child("Photos");
+
+var plantsArray = [];
+
+var plantlist = document.getElementById("plantlist");
+
+var submitButton = document.getElementById("submitPlant");
+var updateavail = document.getElementById("updateavail");
+var submitSignIn = document.getElementById("submitSignIn");
+var submitNewUser = document.getElementById("submitNewUser");
+var currUserLabel = document.getElementById("currUserLabel");
+
+var currInfoWindow;
+var currentUser;
+
+var signOutButton = document.getElementById("signOutButton");
+var signInUserButton = document.getElementById("signInUserButton");
+var signUpUserButton = document.getElementById("signUpUserButton");
+var showPlantsButton = document.getElementById("showPlantsButton");
+
+//AREAS
+var newPlantArea = document.getElementById("newplantarea");
+var signInArea = document.getElementById("signinarea");
+var newUserArea = document.getElementById("newuserarea");
+
+newUserArea.style.display = "none";
+newPlantArea.style.display = "none";
+signInArea.style.display = "none";
+
+
+//USER UPDATES
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+  	currentUser = user;
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    currUserLabel.innerHTML = user.email;
+	signInUserButton.style.display = "none";
+	signUpUserButton.style.display = "none";
+	signInArea.style.display = "none";
+    newUserArea.style.display = "none";
+	signOutButton.style.display = "";	
+	showPlantsButton.style.display = "";
+	
+
+  } else {
+    currentUser = null;
+    showPlantsButton.style.display = "none";
+    signInUserButton.style.display = "";
+    signUpUserButton.style.display = "";
+    currUserLabel.innerHTML = "";
+    signOutButton.style.display = "none";
+    newPlantArea.style.display = "none";
+  }
 });
 
-var db = admin.database();
-var ref = db.ref("/");
-var plantsRef = ref.child("plants");
-var usersRef = ref.child("users");
 
-app.use(bodyParser.urlencoded({extended:true}));
-
-var plantsArr = [];
-var usersArr = [];
-
-plantsRef.orderByChild("plantname").on("child_added", function(snapshot, prevChildKey) {
-	var plantname = snapshot.val().plantname;
-	var url = snapshot.val().imageurl;
-	var sciname = snapshot.val().scientificname;
-	var plantEntry = {
-		name: plantname,
-		imageurl: url,
-		scientificname: sciname,
-		key: snapshot.key
-	};
-	plantsArr.push(plantEntry);
+signInUserButton.addEventListener("click", function(){
+	newPlantArea.style.display = "none";
+	signInArea.style.display = "";
+	newUserArea.style.display = "none";
 });
 
-usersRef.orderByChild("username").on("child_added", function(snapshot, prevChildKey) {
-	var userEntry = {
-		username: snapshot.val().username
-	};
-	usersArr.push(userEntry);
+showPlantsButton.addEventListener("click", function(){
+	newPlantArea.style.display = "";
+	signInArea.style.display = "none";
+	newUserArea.style.display = "none";
 });
 
-app.get("/", function(req, res){
- 	res.render("landing.ejs", {plantsArr: plantsArr});
+signUpUserButton.addEventListener("click", function(){
+	newUserArea.style.display = "";
+	newPlantArea.style.display = "none";
+	signInArea.style.display = "none";
 });
 
+submitButton.addEventListener("click", function(){
+	var filesSelected = document.getElementById("fileInput").files;
+	var fileToLoad = filesSelected[0];
+	var postKey = plantsRef.push().key;
+	var filename = fileToLoad.name;
+	var extension = filename.substr(filename.lastIndexOf('.')+1);
+	var path = postKey + "." + extension;
+	var fileRef = photosRef.child(path);
 
-//JSON ENDPOINT FOR ALL PLANT DATA
-app.get("/json", function(req,res){
-	res.send(JSON.stringify(plantsArr));
-});
+	fileRef.put(fileToLoad).then(function(snapshot) {
+  		console.log('Uploaded a blob or file!');
+  		var imgurl = snapshot.downloadURL;
+  		var plantObject = {
+			plantName: document.forms["plantform"]["plantname"].value,
+			sciName: document.forms["plantform"]["sciname"].value,
+			imgurl: imgurl,
+			userId: currentUser.uid,
+			longitude: document.forms["plantform"]["longitude"].value,
+			latitude: document.forms["plantform"]["latitude"].value,
+			desc: document.forms["plantform"]["desc"].value
+		};
 
-app.get("/newplant", function(req, res){
-	res.render("newplant.ejs");
-});
-
-app.post("/newplant", function(req, res){
-	var name = req.body.plantname;
-	var sciname = req.body.sciname;
-	var url = req.body.imgurl;
-	plantsRef.push({
-		plantname: name,
-		scientificname: sciname,
-		imageurl: url
+		var update = {};
+		update[postKey] = plantObject;
+		console.log(plantsRef.update(update));
 	});
-	res.redirect("/");
+
+
 });
 
-app.get("/plant/:id", function(req, res){
-	var requestedId = req.params.id;
-	var specificRef = plantsRef.child(requestedId);
-	specificRef.once("value", function(snapshot){
-		res.render("plant.ejs", {plantInfo: snapshot.val()});
+
+//NEW USER SIGN UP
+submitNewUser.addEventListener("click", function(){
+	var newEmail = document.forms["newuserform"]["email"].value;
+	var newPassword = document.forms["newuserform"]["password"].value;
+	firebase.auth().createUserWithEmailAndPassword(newEmail, newPassword).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // ...
 	});
 });
 
 
-//JSON ENDPOINT FOR SPECIFIC PLANT ID
-app.get("/plant/:id/json", function(req, res){
-	var specificRef = plantsRef.child(req.params.id);
-	specificRef.once("value", function(snapshot){
-		res.send(snapshot.val());
+//EXISTING USER SIGN IN
+submitSignIn.addEventListener("click", function(){
+	var email = document.forms["signinform"]["email"].value;
+	var password = document.forms["signinform"]["password"].value;
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // ...
 	});
 });
 
-app.get("/newuser", function(req, res){
-	res.render("newuser.ejs");
-});
 
-app.post("/newuser", function(req,res){
-	var username = req.body.username;
-	var password = req.body.password;
-	usersRef.push({
-		username: username,
-		password: password
+//SIGN OUT
+signOutButton.addEventListener("click", function(){
+	firebase.auth().signOut().then(function(){
+		signInUserButton.style.display = "";
+		signUpUserButton.style.display = "";
+		currUser.innerHTML = "";
+		signOutButton.style.display = "none";
+	}).catch(function(error){
+
 	});
-	res.redirect("/users");
 });
 
-app.get("/users", function(req, res){
-	res.render("users.ejs", {usersArr: usersArr});
+var initialValuesSet = false;
+
+plantsRef.on('value', function(snapshot){
+	if(!initialValuesSet){
+		snapshot.forEach(function(data){
+		var plantObject = {
+			plantName: data.val().plantName,
+			sciName: data.val().sciName,
+			longitude: data.val().longitude,
+			latitude: data.val().latitude,
+			desc: data.val().desc,
+			userId: data.val().userId,
+		};
+		var position = {lat: parseFloat(plantObject.latitude), lng: parseFloat(plantObject.longitude)}
+			plantsArray.push(plantObject);
+			createMarker(plantObject);
+		});
+		displayPlants();
+		initialValuesSet = true;
+	} else {
+		updateavail.style.visibility = "visible";
+	}
+	
 });
 
-//JSON ENDPOINT FOR USER DATA
-app.get("/users/json", function(req,res){
-	res.send(JSON.stringify(usersArr));
-});
 
-app.listen(3000, function(){
-    console.log("Scavenger server has started");
-});
+function createMarker(plantObject) {
+	var position = {lat: parseFloat(plantObject.latitude), lng: parseFloat(plantObject.longitude)};
+	var marker = new google.maps.Marker({
+		position: position,
+		map: map,
+		title: plantObject.plantName
+	});
+	marker.addListener('mouseover', function(){
+		currInfoWindow.setContent(marker.title);
+		currInfoWindow.open(map, marker);
+	});
+	marker.addListener('mouseout', function(){
+		currInfoWindow.close();
+	});
+}
+
+function displayPlants(){
+	plantsArray.forEach(function(plant){
+		var node = document.createElement("li");
+		var textnode = document.createTextNode(plant.plantName);
+		node.appendChild(textnode);
+		plantlist.appendChild(node);
+	});
+}
+
+
+var map;
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: -34.397, lng: 150.644},
+	  zoom: 4
+	});
+
+	var infoWindow = new google.maps.InfoWindow({map: map});
+
+	currInfoWindow = new google.maps.InfoWindow({map: map});
+	currInfoWindow.close();
+
+	if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+            document.forms["plantform"]["longitude"].value = pos.lng;
+            document.forms["plantform"]["latitude"].value = pos.lat;
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+}
+
 
